880edfaec44d7c5cc4bcefdc93d6e5df939aa0fd
Eric X <eric-1@yandex.ru>
2019-12-01 02:02:46 +0400

    1

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@
 #define BUT_DELAY     500
 
 void aWrite(uint8_t, int16_t);
-void rgb_blink(void);
-void tick(void);
+void rgb_tick(void);
+void white_tick(void);
 void white_event(param_value_t);
 void rgb_event(param_value_t);
 void butPressed(uint32_t);
@@ -23,7 +23,7 @@ void rgbSetColor(int16_t, int16_t, int16_t);
 
 RCSwitch mySwitch = RCSwitch();
 
-Ticker timer;
+Ticker white_timer;
 Ticker rgb_timer;
 
 uint8_t leds_arr[3] = { BLUE_PIN, GREEN_PIN, RED_PIN };
@@ -73,7 +73,7 @@ void setup (void) {
 
   analogWriteFreq(500);
 
-  timer.attach_ms(whiteTimerPeriod, tick);
+  white_timer.attach_ms(whiteTimerPeriod, white_tick);
 
   mySwitch.enableReceive(RCV433);
 
@@ -88,11 +88,11 @@ void loop (void) {
   if ( !whiteManualMode ) {
     if ( whiteTimerPeriod != 10 ) {
       whiteTimerPeriod = 10;
-      timer.attach_ms(whiteTimerPeriod, tick);
+      white_timer.attach_ms(whiteTimerPeriod, white_tick);
     }
 
     adc_val = analogRead(LIGHT_SENSOR);
-    if ( adc_val < 100 ) {
+    if ( adc_val < 100 && !rgb_mode ) {
       if ( digitalRead(IR_SENSOR) ) {
         lastIRact = millis();
         targetW = LOW_WHITE;
@@ -107,12 +107,12 @@ void loop (void) {
   else {
     if ( whiteTimerPeriod != 2 ) {
       whiteTimerPeriod = 2;
-      timer.attach_ms(whiteTimerPeriod, tick);
+      white_timer.attach_ms(whiteTimerPeriod, white_tick);
     }
   }
 
   if ( curW == 0 && whiteManualMode && !rgb_mode )
-    whiteManualMode = 0;
+    whiteManualMode = false;
 
   if (mySwitch.available()) {
     if ( mySwitch.getReceivedProtocol() == 1 )
@@ -125,7 +125,7 @@ void loop (void) {
 }
 
 void aWrite(uint8_t _pin, int16_t _val) {
-  analogWrite(_pin, pow(1023-_val,2)/1023);
+  analogWrite(_pin, pow(1023-_val,3)/1046529);
 }
 
 void white_event(param_value_t _val) {
@@ -140,21 +140,38 @@ void white_event(param_value_t _val) {
 
 void rgb_event(param_value_t _val) {
   if ( _val == -32768 )
-    rgb_mode = true;
+    rgb_mode = 1;
   else {
     rgb_mode = false;
   }
 }
 
-void tick(void) {
+void white_tick(void) {
   if ( targetW > curW )
     curW++;
   else if ( targetW < curW )
     curW--;
   
   aWrite(WHITE_PIN, curW);
+}
 
-  if ( rgb_mode != 1 ) {
+void rgb_tick(void) {
+  if ( rgb_mode == 1 ) {
+    aWrite(cPin1, 0);
+    aWrite(cPin2, 1023-i);
+    aWrite(cPin3, i);
+
+    i++;
+    if ( i > 1023 ) {
+      i = 0;
+      cPin1 = leds_arr[j++%3];
+      cPin2 = leds_arr[j++%3];
+      cPin3 = leds_arr[j++%3];
+      j++;
+      if ( j > 8 ) j = 0;
+    }
+  }
+  else {
     if ( targetR > curR )
       curR++;
     else if ( targetR < curR )
@@ -173,22 +190,6 @@ void tick(void) {
     aWrite(RED_PIN, curR);
     aWrite(GREEN_PIN, curG);
     aWrite(BLUE_PIN, curB);
-  }
-}
-
-void rgb_blink(void) {
-  aWrite(cPin1, 0);
-  aWrite(cPin2, 1023-i);
-  aWrite(cPin3, i);
-
-  i++;
-  if ( i > 1023 ) {
-    i = 0;
-    cPin1 = leds_arr[j++%3];
-    cPin2 = leds_arr[j++%3];
-    cPin3 = leds_arr[j++%3];
-    j++;
-    if ( j > 8 ) j = 0;
   }
 }
 
@@ -211,16 +212,17 @@ void butPressed(uint32_t _but) {
     case 69106:       // Button 2
       if ( millis() - but2_time > BUT_DELAY ) {
         but2_time = millis();
-        whiteManualMode = true;
         rgb_mode++;
         if ( rgb_mode > 6 )
           rgb_mode = 0;
         switch (rgb_mode) {
           case 0:
             rgbSetColor(0, 0, 0);
+            lastIRact = millis();
             break;
           case 1:
-            rgb_timer.attach_ms(8, rgb_blink);
+            rgb_timer.attach_ms(8, rgb_tick);
+            smarty.setValue(1, -32768);
             break;
           case 2:
             rgbSetColor(0, 0, 1023);
@@ -235,7 +237,7 @@ void butPressed(uint32_t _but) {
             rgbSetColor(0, 1023, 1023);
             break;
           case 6:
-            rgbSetColor(1023, 0, 1023);
+            rgbSetColor(1023, 1023, 0);
             break;
           default:
             break;
@@ -247,6 +249,7 @@ void butPressed(uint32_t _but) {
         but3_time = millis();
         rgb_mode = 0;
         rgbSetColor(0, 0, 0);
+        lastIRact = millis();
       }
       break;
     default:
@@ -255,8 +258,9 @@ void butPressed(uint32_t _but) {
 }
 
 void rgbSetColor(int16_t _r, int16_t _g, int16_t _b) {
-  rgb_timer.detach();
+  rgb_timer.attach_ms(1, rgb_tick);
   targetR = _r;
   targetG = _g;
   targetB = _b;
+  smarty.setValue(1, _r);
 }
